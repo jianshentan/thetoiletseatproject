571af23f065e8ecc20300b2245588f03ffad5d4b
JS <jianshentan@gmail.com>
2015-09-23 00:36:09 -0700

    more stuff

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,23 @@ $( document ).ready( function() {
     });
   });
 
+
+  // sticky nav bar
+  var nav = $(".blog-header");
+  var navPosition = nav.position().top;
+
+  var positionNav = function() {
+    if ($(window).scrollTop() > $(window).height() ) {
+      nav.css("position", "fixed");
+      nav.css("top", "0");
+    } else {
+      nav.css("position", "absolute");
+    }
+  }
+  
+  positionNav();
+  window.onscroll = function() {
+    positionNav();
+  };
+
 });
